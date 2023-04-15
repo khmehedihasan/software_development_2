fe7012c4c89de231f79706a27352b61b6b110993
unknown <mehedihasan47341@gmail.com>
2023-04-16 00:08:01 +0600

    home  page done

---
+++ pos_frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import Layout from '../components/Layout';
+
+
+function Home(){
+
+    return(
+        <Layout>
+            <h1>This is home page.</h1>
+        </Layout>
+    )
+}
+
+export default Home;
